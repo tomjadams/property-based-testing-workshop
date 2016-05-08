ee7162950bf2b52084c2ebb6628a6a14044aeca3
Tom Adams <tomjadams@gmail.com>
2016-05-08 22:00:27 +1000

    Basic properties

--- scala/build.sbt
+++ scala/build.sbt
@@ -6,7 +6,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.6" % "test",
-  "org.scalacheck" %% "scalacheck" % "1.13.0" % "test"
+  "org.scalacheck" %% "scalacheck" % "1.12.5" % "test"
 )
 
 resolvers += Resolver.sonatypeRepo("snapshots")
--- scala/src/test/scala/workshop/exercises/scalacheck/Exercise01.scala
+++ scala/src/test/scala/workshop/exercises/scalacheck/Exercise01.scala
@@ -1,11 +1,12 @@
 package workshop.exercises.scalacheck
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.{Matchers, PropSpec}
 
 // =======
-// Exercise: Write properties that to test a function that adds two integers together.
+// Exercise: Write properties to test a function that adds two integers together.
 // =======
-final class Exercise01 extends FunSpec with Matchers {
+final class Exercise01 extends PropSpec with PropertyChecks with Matchers {
 
   /*
    * We're going to use ScalaCheck for our property testing, it's documentation is here if you need it:
@@ -40,29 +41,55 @@ final class Exercise01 extends FunSpec with Matchers {
    *
    * We'll be exploiting these patterns when writing property-based tests, to make our life simple by making it easier
    * to come up with tests.
-   */
-
-  /*
-   * Here's our addition function from before, we're going to be driving out its implementation again.
+   *
+   *
+   * Here's our addition function from before, we're going to be driving out its implementation again using the tests
+   * below.
    */
   object Addition {
-    def add(a: Int, b: Int): Int = ???
+    def add(a: Int, b: Int): Int = a + b
+//    def add(a: Int, b: Int): Int = ???
   }
 
   /*
-   * The first thing that we need to do is create some random inputs for our addition function. We do this by importing
-   * the `forAll` function like so:
-   */
-
-  import org.scalacheck.Prop.forAll
-
-  /*
+   * The first thing that we need to do is create some inputs for our addition function. We do this using the `forAll`
+   * function. We use this to create the property, as well as to generate inputs that we can use in the test. For
+   * example, we can create a property that says that adding 0 to a number always is just the original number.
    *
+   * forAll((n: Int) => (n + 0) should equal(1))
+   *
+   * Let's see that in an actual test.
    */
-  describe("Adding two integers") {
-    // Add your specs here...
+  property("Adding 0 to a number always returns the original number (std library +)") {
+    forAll((n: Int) => (n + 0) should equal(n))
   }
 
+  /*
+   * Note that we are using the ScalaTest bindings for our properties, what that means is that we can use the matchers
+   * we used earlier in our properties.
+   *
+   * Let's look at testing our addition function.
+   *
+   * Rewrite the property we just saw, but this time using our own addition function from above. You will need to go
+   * back & implement the function to make the test pass, by replacing the `???` with your implementation.
+   */
+  property("Adding 0 to a number always returns the original number") {
+    forAll((n: Int) => Addition.add(n, 0) should equal(n))
+  }
+
+  /*
+   * We can pass more than one
+   *
+   * Write symmetric property that uses the standard subtraction function `-` to check that adding two numbers & taking
+   * them away again is 0.
+   */
+    property("Adding two numbers & taking one away again is the first number") {
+      forAll((a: Int, b: Int) => (Addition.add(a, b) - b) should equal(a))
+    }
+
+  /*
+   * Write a property that
+   */
 
   /*
    * Great! Now go onto `scalatest.Exercise02`.
