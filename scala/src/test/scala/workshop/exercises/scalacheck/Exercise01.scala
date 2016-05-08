0201cb0ce31e84da4da3306b340f38f0fa312bab
Tom Adams <tomjadams@gmail.com>
2016-05-09 08:22:29 +1000

    More wording

--- scala/src/test/scala/workshop/exercises/scalacheck/Exercise01.scala
+++ scala/src/test/scala/workshop/exercises/scalacheck/Exercise01.scala
@@ -10,7 +10,8 @@ final class Exercise01 extends PropSpec with PropertyChecks with Matchers {
 
   /*
    * We're going to use ScalaCheck for our property testing, it's documentation is here if you need it:
-   * https://www.scalacheck.org/documentation.html
+   * https://www.scalacheck.org/documentation.html. If you get stuck, there are some good examples here:
+   * https://github.com/oscarrenalias/scalacheck-examples/blob/master/scalacheck-integration-scalatest/src/test/scala/SimpleSpec.scala
    *
    * When writing property-based tests, like in a lot of software development, it's often helpful to look for patterns,
    * these make writing tests easier. For example, you can write a function that turns a String into UTF-8 encoded byte
@@ -42,7 +43,6 @@ final class Exercise01 extends PropSpec with PropertyChecks with Matchers {
    * We'll be exploiting these patterns when writing property-based tests, to make our life simple by making it easier
    * to come up with tests.
    *
-   *
    * Here's our addition function from before, we're going to be driving out its implementation again using the tests
    * below.
    */
@@ -53,21 +53,29 @@ final class Exercise01 extends PropSpec with PropertyChecks with Matchers {
 
   /*
    * The first thing that we need to do is create some inputs for our addition function. We do this using the `forAll`
-   * function. We use this to create the property, as well as to generate inputs that we can use in the test. For
-   * example, we can create a property that says that adding 0 to a number always is just the original number.
+   * function. This both creates the property, as well as generating inputs that we can use for the test. `forAll`
+   * generates a bunch of random Ints, including edge cases, and run them through our test. This is far more efficient
+   * than writing a bunch of examples manually and hoping to get them right, with enough coverage of the input space.
+   *
+   * Consider an integer for example, how many values do we need to use in order to test a broad spectrum of inputs?
+   * We'd need to test some subset between -2147483648 and 2147483647. There's a lot of scope to get that wrong.
+   * ScalaCheck will take care of generating appropriate inputs for us.
+   *
+   * Here is a property that says that the result of adding 0 to a number is always just the original number.
    *
    * forAll((n: Int) => (n + 0) should equal(1))
    *
-   * Let's see that in an actual test.
+   * What is happening is that ScalaCheck is generating us a bunch of random Int values and feeding them into our
+   * function. It will generate edge cases, like min/max integers, zero, etc. as well as a broad scope of other values.
+   *
+   * Let's see that in a runnable test. Note that we are using the ScalaTest bindings for our properties, what that
+   * means is that we can use the matchers we used earlier in our properties.
    */
   property("Adding 0 to a number always returns the original number (std library +)") {
     forAll((n: Int) => (n + 0) should equal(n))
   }
 
   /*
-   * Note that we are using the ScalaTest bindings for our properties, what that means is that we can use the matchers
-   * we used earlier in our properties.
-   *
    * Let's look at testing our addition function.
    *
    * Rewrite the property we just saw, but this time using our own addition function from above. You will need to go
@@ -78,18 +86,25 @@ final class Exercise01 extends PropSpec with PropertyChecks with Matchers {
   }
 
   /*
-   * We can pass more than one
+   * We can pass more than one argument to our function and ScalaCheck will generate us random values for all of these
+   * parameters.
    *
-   * Write symmetric property that uses the standard subtraction function `-` to check that adding two numbers & taking
-   * them away again is 0.
+   * Pass two Int arguments to our check & write symmetric property that uses the standard subtraction function `-` to
+   * check that adding two numbers & taking one away again is the first number.
    */
     property("Adding two numbers & taking one away again is the first number") {
       forAll((a: Int, b: Int) => (Addition.add(a, b) - b) should equal(a))
     }
 
   /*
-   * Write a property that
+   * We can rewrite that in a couple of different ways.
    */
+  property("2. Adding two numbers & taking one away again is the first number") {
+    forAll((a: Int, b: Int) => (Addition.add(a, b) - a - b) should equal(0))
+  }
+  property("3. Adding two numbers & taking one away again is the first number") {
+    forAll((a: Int, b: Int) => (Addition.add(a, b) - Addition.add(a, b)) should equal(0))
+  }
 
   /*
    * Great! Now go onto `scalatest.Exercise02`.
